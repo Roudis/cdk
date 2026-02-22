78cb52f23f00b347dd21675e25feb7d09837a3c7
roudis <alkisroudis@gmail.com>
2026-02-22 18:32:58 +0100

    Refactor code structure for improved readability and maintainability

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the S3 Bucket
+    new s3.Bucket(this, 'AlkisBucketIdentifier', {
+      bucketName: 'alkis-bucket-cdk', // The physical name you requested
+      versioned: true,             // Good practice for testing
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Deletes bucket when stack is deleted
+      autoDeleteObjects: true,     // Required to delete a non-empty bucket
+    });
   }
 }
